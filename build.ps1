32c023df72447dec7d84c96adab0ac4dce9c93ed
Wes Higbee <wes.mcclure@gmail.com>
2016-10-30 17:24:27 -0400

    Add build script

---
+++ build.ps1
@@ -0,0 +1,3 @@
+docker build -f .\nanoserver\Dockerfile .\nanoserver -t weshigbee/sysinternals
+docker build -f .\windowsservercore\Dockerfile .\windowsservercore -t weshigbee/sysinternals:windowsservercore
+
